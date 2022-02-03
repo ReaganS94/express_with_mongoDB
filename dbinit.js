2c9b12ee96db2dfe38677a32bdbd50dc0d61334f
ReaganS <reagansasan@gmail.com>
2022-02-03 16:29:30 +0100

    added gitignore

--- dbinit.js
+++ dbinit.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(
-    "mongodb+srv://wd22:peakyblinders@wd22exercise.yigwj.mongodb.net/WD22?retryWrites=true&w=majority"
-  );
+  const conn = await mongoose.connect(process.env.MONGO_URI);
 
   console.log(`MongoDB connected ${conn.connection.host}`);
 };
